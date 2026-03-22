a45f809b1bf51034c8557c7136d9179148094223
madhusudhana26 <amsrao2000@gmail.com>
2026-03-22 14:36:29 +0530

    Update service_buggy.cpp
    Fix substring boundary bug

--- service_buggy.cpp
+++ service_buggy.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 string processRequest(string input) {
     if(input.length() > 10)
-        return input.substr(0, 20); // potential bug
+        // return input.substr(0, 20); 
+        return input.substr(0, 10); // potential bug
     return input;
 }
 
